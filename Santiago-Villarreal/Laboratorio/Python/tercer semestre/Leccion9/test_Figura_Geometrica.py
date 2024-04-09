4365c1a9f89988ffb56eee537a0dfe7888305530
Santiago Villarreal <santivilla795@hotmail.com>
2024-04-09 18:30:37 -0300

    Revert "Clase 1 de python terminada"
    This reverts commit 496a9d6e995d56382ebd63f7e021739ed7ed8dcf.

--- Santiago-Villarreal/Laboratorio/Python/tercer semestre/Leccion9/test_Figura_Geometrica.py
+++ Santiago-Villarreal/Laboratorio/Python/tercer semestre/Leccion9/test_Figura_Geometrica.py
@@ -1,18 +1,6 @@
 from Cuadrado import Cuadrado
-from Rectangulo import Rectangulo
 
-cuadrado1 = Cuadrado(5, 'verde')
+cuadrado1 = Cuadrado(5, 'Azul')
 print(cuadrado1.ancho)
 print(cuadrado1.alto)
 print(f'Càlculo del área del cuadrado: {cuadrado1.calcular_area()}')
-
-rectangulo1 = Rectangulo(7, 5, 'azul')
-print(rectangulo1.ancho)
-print(rectangulo1.alto)
-print(f'Cálculo del area del rectangulo: {rectangulo1.calcular_area1()}')
-
-# MRO = Method Resolution Order
-print(Cuadrado.mro())
-
-print(cuadrado1)
-
