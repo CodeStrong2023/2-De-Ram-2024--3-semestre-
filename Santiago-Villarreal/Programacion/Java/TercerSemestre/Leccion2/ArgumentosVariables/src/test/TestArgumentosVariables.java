409375272845618e66664a346d9da28a54ebb8d5
Santiago Villarreal <santivilla795@hotmail.com>
2024-04-11 23:52:11 -0300

    Clase 2 de java (incompleta)

---
+++ Santiago-Villarreal/Programacion/Java/TercerSemestre/Leccion2/ArgumentosVariables/src/test/TestArgumentosVariables.java
@@ -0,0 +1,15 @@
+package TercerSemestre.Leccion2.ArgumentosVariables.src.test;
+
+public class TestArgumentosVariables {
+    public static void main(String[] args) {
+        
+    }
+
+    private static void imprimirNumeros(int ...numeros){
+       for(int i=0; i < numeros.length;i++){
+        System.out.println("Elementos: " + numeros[i]);
+       } 
+       
+    }
+
+}
---
+++ Santiago-Villarreal/Programacion/Java/TercerSemestre/Leccion2/enumeraciones/src/enumeraciones/Dias.java
@@ -0,0 +1,11 @@
+package TercerSemestre.Leccion2.enumeraciones.src.enumeraciones;
+
+public enum Dias {
+    LUNES,
+    MARTES,
+    MIERCOLES,
+    JUEVES,
+    VIERNES,
+    SABADO,
+    DOMINGO;
+}
