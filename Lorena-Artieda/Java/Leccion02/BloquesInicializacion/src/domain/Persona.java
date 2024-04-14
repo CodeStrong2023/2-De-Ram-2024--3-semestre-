9d95eb09530684a9e468fa158c80724c217cbf59
ArtiedaLorena <artiedalorena@gmail.com>
2024-04-14 19:22:46 -0300

    Clase 02 Java

---
+++ Lorena-Artieda/Java/Leccion02/BloquesInicializacion/src/domain/Persona.java
@@ -0,0 +1,37 @@
+package domain;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Persona {
+    private final int idPersona;
+    private static int contadorPersonas;
+
+    //Bloque de inicializacion estatico
+    static{
+        System.out.println("Ejecucion bloque estatico");
+        ++Persona.contadorPersonas;
+    }
+
+
+    //Bloque de inicializacion dinamico
+    {
+        System.out.println("Ejecucion de bloque No estatico");
+        this.idPersona=Persona.contadorPersonas++;//Incrementamos el atributo
+    }
+    //Los bloques de inicializacion se ejecutan antes del constructor
+    public Persona() {
+        System.out.println("Ejecucion del constructor");
+    }
+
+    public int getIdPersona(){
+        return this.idPersona;
+    }
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "idPersona=" + idPersona +
+                '}';
+    }
+
+
+}
