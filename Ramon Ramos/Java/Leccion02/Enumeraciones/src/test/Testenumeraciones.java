1880a55fa6ab9a7586c95bbcb31990ce59496959
RamonRamos1 <108014511+RamonRamos1@users.noreply.github.com>
2024-04-18 12:45:36 -0300

    Java Clase 02
    Argumentos y Enumeraciones

---
+++ Ramon Ramos/Java/Leccion02/Enumeraciones/src/test/Testenumeraciones.java
@@ -0,0 +1,9 @@
+package test;
+
+import enumeraciones.Dias;
+    
+public class Testenumeraciones {
+    public static void main(String[] args) {
+        System.out.println("Dia 1: "+Dias.Domingo);
+    }
+}
