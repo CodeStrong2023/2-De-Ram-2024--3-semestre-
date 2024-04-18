ea648866009de421da67257352ddbdf86f3395ad
Santiago Villarreal <santivilla795@hotmail.com>
2024-04-18 14:56:32 -0300

    Revert "Merge pull request #22 from CodeStrong2023/develop"
    This reverts commit 5bc5c2b284b413af9bdc25cbfe6fc7e20b54d5f1, reversing
    changes made to df2245c3664ad444730fda9133c3f3620e4cd4a0.

--- Luis Mera/java/Leccion1/mundopc/src/main/java/ar/com/system2023/mundopc/Main.java
+++
@@ -1,7 +0,0 @@
-package ar.com.system2023.mundopc;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- Ramon Ramos/Java/Leccion02/Enumeraciones/src/test/Testenumeraciones.java
+++
@@ -1,9 +0,0 @@
-package test;
-
-import enumeraciones.Dias;
-    
-public class Testenumeraciones {
-    public static void main(String[] args) {
-        System.out.println("Dia 1: "+Dias.Domingo);
-    }
-}
