652d8568c2489bbf41e455232d01cf4ca1e36677
Santiago Villarreal <santivilla795@hotmail.com>
2024-04-18 15:52:02 -0300

    Clase 2 de python completa

--- Santiago-Villarreal/Laboratorio/Python/tercer semestre/Leccion10/MiClase.py
+++ Santiago-Villarreal/Laboratorio/Python/tercer semestre/Leccion10/MiClase.py
@@ -5,8 +5,36 @@ class MiClase:
     def __init__(self, variable_instancia): #da otros valores
         self.variable_instancia = variable_instancia
 
+
+    @staticmethod
+    def metodo_estatico():
+        print(MiClase.variable_clase)
+
+    @classmethod
+    def metodo_clase(cls):
+        print(cls.variable_clase)
+    
+    def metodo_instancia(self):
+        self.metodo_clase()
+        self.metodo_estatico()
+        print(self.variable_clase)
+        print(self.variable_instancia)
+
+
 print(MiClase.variable_clase)
 miClase1 = MiClase('Esta es una variable de instancia')
 print(miClase1.variable_instancia)
 miClase2 = MiClase('Esta es la otra prueba de variable de instancia')
 print(miClase2.variable_clase)
+
+MiClase.variable_clase2 = 'valor de variable de clase 2'
+print(MiClase.variable_clase2)
+print(miClase1.variable_clase2)
+print(miClase2.variable_clase2)
+
+MiClase.metodo_estatico()
+
+MiClase.metodo_clase()
+miObjeto1 = MiClase('variable de instancia')
+miObjeto1.metodo_clase()
+miObjeto1.metodo_instancia()
