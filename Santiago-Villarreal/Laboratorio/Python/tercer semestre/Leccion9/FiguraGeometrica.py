8776ec09982c3e20da69486930dd79898f210424
Santiago Villarreal <santivilla795@hotmail.com>
2024-04-17 22:26:21 -0300

    Clase 2 de Python (1er mitad)

--- Santiago-Villarreal/Laboratorio/Python/tercer semestre/Leccion9/FiguraGeometrica.py
+++ Santiago-Villarreal/Laboratorio/Python/tercer semestre/Leccion9/FiguraGeometrica.py
@@ -1,7 +1,20 @@
+from abc import ABC, abstractmethod
+# ABC = abstract base class
+
+
+
 class FiguraGeometrica:
     def __init__(self, ancho, alto):
-        self.ancho = ancho
-        self.alto = alto
+        if self._validar_valores(ancho):
+            self._ancho = ancho
+        else:
+            self._ancho = 0
+            print(f'valor erróneo para el ancho: {ancho}')
+        if self._validar_valores(alto):
+            self._alto = alto
+        else:
+            self._alto = 0
+            print(f'valor erróneo para el ancho: {alto}')
 
     @property
     def ancho(self):
@@ -9,7 +22,10 @@ class FiguraGeometrica:
 
     @ancho.setter
     def ancho(self, ancho):
-        self._ancho = ancho
+        if self._validar_valores(ancho):
+            self._ancho = ancho
+        else:
+            print(f'valor erróneo ancho: {ancho}')
 
     @property
     def alto(self):
@@ -17,7 +33,18 @@ class FiguraGeometrica:
 
     @alto.setter
     def alto(self, alto):
-        self._alto = alto
+        if self._validar_valores(alto):
+            self._alto = alto
+        else:
+            print(f'valor erróneo ancho: {alto}')
+
+    @abstractmethod
+    def calcular_area(self):
+        pass
 
     def __str__(self):
         return f'FiguraGeométrica [Ancho: {self._ancho}, Alto: {self._alto}]'
+    
+    def _validar_valores(self, valor): #metodo encapsulado
+        return True if 0 < valor < 10 else False
+    
