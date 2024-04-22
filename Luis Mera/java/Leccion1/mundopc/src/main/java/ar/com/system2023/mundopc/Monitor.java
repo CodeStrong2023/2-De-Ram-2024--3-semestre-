c87667013cc11b37cc422082b3080ec606d3c6f2
Luis Mera <luismera86@gmail.com>
2024-04-22 20:04:35 -0300

    clase comuputadora

--- Luis Mera/java/Leccion1/mundopc/src/main/java/ar/com/system2023/mundopc/Monitor.java
+++ Luis Mera/java/Leccion1/mundopc/src/main/java/ar/com/system2023/mundopc/Monitor.java
@@ -1,24 +1,61 @@
 package ar.com.system2023.mundopc;
 
 public class Monitor {
+
   private final int idMonitor;
   private String marca;
   private double tamanio;
   private static int contadorMonitores;
 
   private Monitor() {
-    this.idMonitor = ++contadorMonitores;
+    this.idMonitor = ++Monitor.contadorMonitores;
   }
 
   public Monitor(String marca, double tamanio) {
     this();
     this.marca = marca;
     this.tamanio = tamanio;
-    
+  }
+
+  public int getIdMonitor() {
+    return idMonitor;
+  }
+
+  public String getMarca() {
+    return marca;
+  }
+
+  public void setMarca(String marca) {
+    this.marca = marca;
+  }
+
+  public double getTamanio() {
+    return tamanio;
+  }
+
+  public void setTamanio(double tamanio) {
+    this.tamanio = tamanio;
+  }
+
+  public static int getContadorMonitores() {
+    return contadorMonitores;
+  }
+
+  public static void setContadorMonitores(int contadorMonitores) {
+    Monitor.contadorMonitores = contadorMonitores;
   }
 
   @Override
   public String toString() {
-    return "Monitor{" + "idMonitor=" + idMonitor + ", marca=" + marca + ", tamanio=" + tamanio + '}';
+    return (
+      "Monitor{" +
+      "idMonitor=" +
+      idMonitor +
+      ", marca=" +
+      marca +
+      ", tamanio=" +
+      tamanio +
+      '}'
+    );
   }
 }
