6eacdd3b227cf16469c7e71acfaf8c1e598773b4
Santiago Villarreal <santivilla795@hotmail.com>
2024-04-24 13:11:51 -0300

    Clase 3 python (incompleta)

---
+++ Santiago-Villarreal/Laboratorio/Python/tercer semestre/Leccion11/Orden.py
@@ -0,0 +1,40 @@
+from Producto import Producto
+
+
+class Orden:
+    contador_ordenes = 0
+
+    def __init__(self, productos):
+        Orden.contador_ordenes += 1 
+        self._productos = list(productos)
+
+def agregar_producto(self, producto):
+    self._productos.append(producto) # para agregar nuevo producto
+
+def calcular_total(self):
+    total = 0 # variable temporal
+
+    for producto in self._productos:
+        total += producto.precio
+    return total 
+
+def __str__(self):
+    productos_str = ''
+    for producto in self._productos:
+        productos_str += producto.__str__()+'|'
+    return f'Orden: {self.id_orden}, \nProducto: {productos_str}'
+
+if __name__ == '__main__':
+    producto1 = Producto('Camiseta',100.00)
+    producto2 = Producto('Pantalon', 150.00)
+    producto3 = Producto('Zapatillas', 200.00)
+    producto4 = Producto('Campera', 175.00)
+    productos1 = [producto1, producto2] 
+    productos2 = [producto3, producto4]
+    orden1 = Orden(productos1)
+    print(orden1)
+    orden2 = Orden(productos2)
+    print(orden2)
+
+# Modificar la orden2 ingresnado nuevos productos con sus nombre y precios
+# Crear una lista de productos y agregarla a la orden2
---
+++ Santiago-Villarreal/Laboratorio/Python/tercer semestre/Leccion11/Producto.py
@@ -0,0 +1,34 @@
+class Producto:
+    contador_productos = 0 # variable de clase
+
+    def __init__(self, nombre, precio):
+        Producto.contador_productos += 1
+        self._id_producto = Producto.contador_productos # asignacion desde la variable de clase
+        self._precio = precio
+# getter aand setters
+@property 
+def nombre(self):
+    return self._nombre
+
+@nombre.setter
+def nombre(self, nombre):
+    self._nombre = nombre
+
+@property
+def precio(self):
+    return self._precio
+
+@precio.setter
+def precio(self, precio):
+    self._precio = precio
+# metodo str
+def __str__(self):
+    return f'Id Producto: {self._id_producto}, Nombre: {self._nombre}, Precio: {self._precio}'
+
+if __name__ == '__main__':
+    producto1 = Producto('camiseta', 100.00)
+    print(producto1)
+    producto2 = Producto('Pantalon', 150.00)
+    print(producto2)
+
+
