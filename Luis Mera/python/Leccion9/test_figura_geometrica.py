69bb1bfc5ed8f252b9a9a9af79abd93a18de4540
Luis Mera <luismera86@gmail.com>
2024-04-26 01:17:44 -0300

    .

---
+++ Luis Mera/python/Leccion9/test_figura_geometrica.py
@@ -0,0 +1,14 @@
+
+from Curadrado import Cuadrado
+
+cuadrado1 = Cuadrado(5, "Azul")
+
+print(cuadrado1.ancho)
+print(cuadrado1.alto)
+print(f"El area del cuadrado es {cuadrado1.calcular_area()}")
+
+# Método MRO
+#print(Cuadrado.mro())
+
+print(cuadrado1)
+
