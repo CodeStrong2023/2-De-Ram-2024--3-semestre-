0c619ed971037b694ccab90a1f4bcfc0a0f60f0b
Luis Mera <luismera86@gmail.com>
2024-04-26 01:23:10 -0300

    .

--- Luis Mera/python/Leccion9/test_figura_geometrica.py
+++ Luis Mera/python/Leccion9/test_figura_geometrica.py
@@ -1,5 +1,5 @@
-
 from Curadrado import Cuadrado
+from Rectangulo import Rectangulo
 
 cuadrado1 = Cuadrado(5, "Azul")
 
@@ -8,7 +8,10 @@ print(cuadrado1.alto)
 print(f"El area del cuadrado es {cuadrado1.calcular_area()}")
 
 # Método MRO
-#print(Cuadrado.mro())
+# print(Cuadrado.mro())
 
 print(cuadrado1)
 
+rectangulo1 = Rectangulo(3,8, "verde")
+print(f"Calculo del area de rectangulo: {rectangulo1.calcular_area()}")
+print(rectangulo1)
