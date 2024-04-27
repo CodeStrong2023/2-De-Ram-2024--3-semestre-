e245aff8f48cf56ff99866447e6b6cb0b2f3ba09
Luis Mera <luismera86@gmail.com>
2024-04-26 23:38:38 -0300

    .

---
+++ Luis Mera/javascript/TSLeccion03/01-03-funciones.js
@@ -0,0 +1,20 @@
+function miFuncion(a, b) {
+  console.log(`Suma ${a + b}`);
+}
+
+miFuncion(3, 5);
+
+function miFuncion2(a, b) {
+  return a + b;
+}
+
+let resultado = miFuncion2(3, 5);
+
+console.log(resultado);
+
+// Funciones flecha
+
+const sumarFuncionFlecha = (a, b) => a + b;
+console.log(sumarFuncionFlecha(2, 5));
+
+let resultado2 = sumarFuncionFlecha(3,4);
