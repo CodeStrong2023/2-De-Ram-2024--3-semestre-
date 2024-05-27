7baf2817ecd6e79892536e86f0dba54264752720
Santiago Villarreal <santivilla795@hotmail.com>
2024-05-27 20:57:13 -0300

    Clase 8 de javascript completa

--- Santiago-Villarreal/Laboratorio/JavaScript/3/tsLeccion05/05-03-clases.js
+++ Santiago-Villarreal/Laboratorio/JavaScript/3/tsLeccion05/05-03-clases.js
@@ -1,7 +1,21 @@
 class Persona{ //clase padre
+
+    static contadorPersonas = 0;
+    // email = 'Valor default email';
+    static get MAX_OBJ(){
+        return 5;
+    }
+
     constructor(nombre, apellido){
         this._nombre = nombre;
         this._apellido = apellido;
+        if(Persona.contadorPersonas < Persona.MAX_OBJ){
+            this.idPersona = ++Persona.contadorPersonas;
+        } else {
+            console.log('Se ha superadoel máximo de objetos permitidos');
+        }
+        
+        // console.log('Se incrementa el contador: ' + Persona.contadorObjetosPersona);
     }
 
     get nombre(){
@@ -21,11 +35,19 @@ class Persona{ //clase padre
     }
 
     nombreCompleto(){
-        return this._nombre+' '+this._apellido;
+        return this.idPersona+ ' ' + this._nombre + ' ' + this._apellido;
     }
 
     toString(){
         return this.nombreCompleto();
+    }
+
+    static saludar(){
+        console.log("saludos desde el método static")
+    }
+
+    static saludar2(persona){
+        console.log(persona.nombre + ' ' + persona.apellido);
     }
 
 }
@@ -71,3 +93,34 @@ console.log(empleado1.nombreCompleto());
 // Object.prototype.toString
 console.log(empleado1.toString());
 console.log(persona1.toString());
+
+// persona1.saludar(); no se utiliza desde el objeto
+
+Persona.saludar();
+Persona.saludar2(persona1);
+
+Empleado.saludar();
+Empleado.saludar2(empleado1);
+
+// console.log(persona1.contadorObjetosPersona);
+
+console.log(Persona.contadorPersonas);
+console.log(Empleado.contadorPersonas);
+ 
+// console.log(persona1.email);
+// console.log(empleado1.email);
+// console.log(Persona.email); No estatico, no se puede acceder desde la clase
+console.log(persona1.toString());
+console.log(persona2.toString());
+console.log(empleado1.toString());
+console.log(Persona.contadorPersonas);
+let persona3 = new Persona('Carla', 'Pertosi');
+console.log(persona3.toString());
+console.log(Persona.contadorPersonas);
+console.log(Persona.MAX_OBJ);
+// Persona.MAX_OBJ = 10; // no se modifica
+
+let persona4 = new Persona('Franco', 'Díaz');
+console.log(persona4.toString());
+let persona5 = new Persona('Liliana', 'Paz');
+console.log(persona5.toString());
