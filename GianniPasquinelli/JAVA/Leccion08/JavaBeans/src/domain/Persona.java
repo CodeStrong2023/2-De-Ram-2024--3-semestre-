a09ce663dac7bfc15b3b0212fd2b6abe0f7e9f7a
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2024-06-12 20:28:00 -0300

    JAVA clase 08

---
+++ GianniPasquinelli/JAVA/Leccion08/JavaBeans/src/domain/Persona.java
@@ -0,0 +1,43 @@
+package domain;
+
+import java.io.Serializable;
+
+public class Persona implements Serializable {
+    private String nombre;
+    private String apellido;
+
+    // El contructor vació es obligatorio
+    public Persona() {
+
+    }
+
+    public Persona(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Persona{");
+        sb.append("nombre='").append(nombre).append('\'');
+        sb.append(", apellido='").append(apellido).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
