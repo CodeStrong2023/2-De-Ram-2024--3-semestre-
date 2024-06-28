e810b8e768eb972c007726c2b45331623404a64f
Lorena Artieda <76899464+ArtiedaLorena@users.noreply.github.com>
2024-06-28 19:12:24 -0300

    Se sube Clase 11-a Capa de Datos Parte 3

--- Lorena-Artieda/Python/Leccion10/Loggin/Conexion.py
+++ Lorena-Artieda/Python/Leccion10/Loggin/Conexion.py
@@ -1,8 +1,6 @@
 import sys
 import psycopg2 as bd
 from logger_base import log
-from psycopg2 import pool
-
 
 class Conexion:
     _DATABASE = 'test_bd'
@@ -10,40 +8,43 @@ class Conexion:
     _PASSWORD = 'admin'
     _DB_PORT = '5432'
     _HOST = '127.0.0.1'
-    _conexion= None
-    _cursor= None
+    _conexion = None
+    _cursor = None
 
     @classmethod
     def obtenerConexion(cls):
         if cls._conexion is None:
             try:
-                cls._conexion= bd.connect(host= cls._HOST,
-                                          user=cls._USERNAME,
-                                          password=cls._PASSWORD,
-                                          port=cls._DB_PORT,
-                                          database=cls._DATABASE)
+                cls._conexion = bd.connect(
+                    host=cls._HOST,
+                    user=cls._USERNAME,
+                    password=cls._PASSWORD,
+                    port=cls._DB_PORT,
+                    database=cls._DATABASE
+                )
                 log.debug(f"Conexion exitosa de la conexion: {cls._conexion}")
-
-                return cls._conexion
             except Exception as e:
-                log.error(f'Ocurrio un error: {e}')
-                sys.exit()
-            else:
-                return cls._conexion
+                log.error(f'Ocurrió un error al obtener la conexión: {e}')
+                cls._conexion = None
+        return cls._conexion
 
     @classmethod
     def obtenerCursor(cls):
-        if cls._cursor is None:
+        conexion = cls.obtenerConexion()
+        if conexion is not None:
             try:
-                cls._cursor = cls.obtenerConexion().cursor()
+                cls._cursor = conexion.cursor()
+                log.debug(f"Cursor obtenido: {cls._cursor}")
                 return cls._cursor
             except Exception as e:
-                log.error(f'Ocurrió un error: {e}')
-                sys.exit()
-        else:
-            return cls._cursor
-
+                log.error(f'Ocurrió un error al obtener el cursor: {e}')
+                cls._cursor = None
+        return cls._cursor
 
 if __name__ == '__main__':
-    Conexion.obtenerConexion()
-    Conexion.obtenerCursor()
+    conexion = Conexion.obtenerConexion()
+    cursor = Conexion.obtenerCursor()
+    if conexion:
+        log.debug('Conexion obtenida exitosamente')
+    if cursor:
+        log.debug('Cursor obtenido exitosamente')
--- Lorena-Artieda/Python/Leccion10/Loggin/PersonaDAO.py
+++ Lorena-Artieda/Python/Leccion10/Loggin/PersonaDAO.py
@@ -1,4 +1,9 @@
-class PersonaDAO:
+from Conexion import Conexion
+from capa_datos_persona.Persona import Persona
+from logger_base import log
+
+
+class PersonaDao:
     """
     DAO: Data Access Object
     CRUD:
@@ -11,3 +16,73 @@ class PersonaDAO:
     _INSERTAR = 'INSERT INTO persona(nombre, apellido, email) VALUES (%s, %s, %s)'
     _ACTUALIZAR = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
     _ELIMINAR = 'DELETE FROM persona WHERE id_persona=%s'
+
+    # Definicion de metodos de clase
+    @classmethod
+    def seleccionar(cls):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                cursor.execute(cls._SELECCIONAR)
+                registros = cursor.fetchall()
+                personas = []  # lista
+                for registro in registros:
+                    persona = Persona(registro[0], registro[1], registro[2], registro[3])
+                    personas.append(persona)
+                return personas
+
+    @classmethod
+    def insertar(cls, persona):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                valores = (persona.nombre, persona.apellido, persona.email)
+                cursor.execute(cls._INSERTAR, valores)
+                log.debug(f'Persona insertada: {persona}')
+                return cursor.rowcount
+
+
+    @classmethod
+    def actualizar(cls, persona):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                valores = (persona.nombre, persona.apellido, persona.email, persona.id_persona)
+                cursor.execute(cls._ACTUALIZAR, valores)
+                log.debug(f'Persona actualizada: {persona}')
+                return cursor.rowcount
+
+    @classmethod
+    def eliminar(cls, persona):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                valores = (persona._id_persona,)
+                cursor.execute(cls._ELIMINAR, valores)
+                log.debug(f'Los objetos eliminados son: {persona}')
+                return cursor.rowcount
+
+
+
+
+if __name__ == '__main__':
+    # Eliminar un registro
+    '''
+    persona1 = Persona(id_persona=2)
+    personas_eliminadas = PersonaDao.eliminar(persona1)
+    log.debug(f'Personas eliminadas: {personas_eliminadas}')
+    '''
+
+    # Actualizar un registro
+    '''
+    persona1 = Persona(1, 'Juan José', 'Pena', 'jjpena@mail.com')
+    personas_actualizadas = PersonaDao.actualizar(persona1)
+    log.debug(f'Personas actualizadas: {personas_actualizadas}')
+    '''
+
+    '''# Insertar un registro
+
+    persona1 = Persona(nombre='Homero', apellido='Ramos', email='hramos@mail.com')
+    personas_insertadas = PersonaDao.insertar(persona1)
+    log.debug(f'Personas insertadas: {personas_insertadas}')
+    '''
+    # Seleccionar objetos
+    personas = PersonaDao.seleccionar()
+    for persona in personas:
+        log.debug(persona)
